b3968bfbc4fa94b9df6d15b7e6c7198955c02694
ronaldo <ronaldoskibinski@outlook.com>
2021-09-11 21:42:25 -0300

    Feita a tabela Usuarios

---
+++ Documents/TUDO DE PROGRAMAÇÃO/Angular/farmacia-terezinha/src/database-queries/queries.sql
@@ -0,0 +1,11 @@
+create database farmaciaterezinha;
+
+use farmaciaterezinha;
+
+create table usuario (
+	nome varchar(30),
+	telefone varchar(14),
+	email varchar(100),
+	endereco varchar(100),
+	senha varchar(100)
+);
